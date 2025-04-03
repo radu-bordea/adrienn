f6b011944a6557543b4443a99dd7fc67f001cd27
radu-bordea <radu.bordea.dev@gmail.com>
2025-04-03 14:22:38 +0200

    create controllers for getProducts, getProduct and postProducts methods

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,36 +1,17 @@
 const express = require("express");
-const Product = require("../models/productModel");
+const { createProduct, getProducts, getProduct } = require("../controllers/productController");
+
 
 const router = express.Router();
 
 // GET all products
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all products" });
-});
+router.get("/", getProducts);
 
 // GET a single product
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single product" });
-});
+router.get("/:id", getProduct);
 
 // POST a new product
-router.post("/", async (req, res) => {
-  const { name, price, description, category, stock, imageUrl } = req.body;
-
-  try {
-    const product = await Product.create({
-      name,
-      price,
-      description,
-      category,
-      stock,
-      imageUrl,
-    });
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createProduct);
 
 // DELETE a product
 router.delete("/:id", (req, res) => {
