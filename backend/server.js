4c6ca6f70c167cd8675828838404f9d1069f4d42
radu-bordea <radu.bordea.dev@gmail.com>
2025-04-03 09:54:53 +0200

    create express router and add products routes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
-const express = require("express")
-require("dotenv").config()
+require("dotenv").config();
+const express = require("express");
+const productRoutes = require("./routes/products");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next()
-})
-
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app!" });
-});
+app.use("/api/products", productRoutes);
 
 // listen for request
 app.listen(process.env.PORT, () => {
-    console.log("listening on port 4000");
-    
-})
+  console.log("listening on port 4000");
+});
